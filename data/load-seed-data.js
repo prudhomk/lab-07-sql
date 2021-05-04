5a05e35e5797568604536d9d8482fd1c5f47f2be
Kalan Prudhomme <cleaver9@gmail.com>
2021-05-04 13:52:06 -0700

    data live tests pass

--- data/load-seed-data.js
+++ data/load-seed-data.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-console */
 import client from '../lib/client.js';
 // import our seed data:
-import cats from './cats.js';
+import strongest from './data.js';
 
 run();
 
@@ -10,12 +10,12 @@ async function run() {
   try {
 
     await Promise.all(
-      cats.map(cat => {
+      strongest.map(being => {
         return client.query(`
-          INSERT INTO cats (name, type, url, year, lives, is_sidekick)
-          VALUES ($1, $2, $3, $4, $5, $6);
+          INSERT INTO strongest (name, type, description, power, is_good)
+          VALUES ($1, $2, $3, $4, $5);
         `,
-        [cat.name, cat.type, cat.url, cat.year, cat.lives, cat.isSidekick]);
+        [being.name, being.type, being.description, being.power, being.isGood]);
       })
     );
     
